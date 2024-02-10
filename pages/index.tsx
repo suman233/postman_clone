d9aba43d98e70edb05e076e9f73a62e0c9a0d47d
suman233 <77207981+suman233@users.noreply.github.com>
2024-02-11 00:47:44 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { Container, Typography } from "@mui/material";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
